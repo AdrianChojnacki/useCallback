50815915c245fe4e40325948a1c50c2d9f21dbb7
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-20 20:24:41 +0100

    added counter.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Counter from './Counter';
+
 import './App.css';
 
 const App = () => {
+  const [firstCounter, setFirstCounter] = useState(0);
+  const [secondCounter, setSecondCounter] = useState(0);
+
+  const increaseFirstCounter = () =>
+    setFirstCounter(firstCounter + 1);
+
+  const increaseSecondCounter = () =>
+    setSecondCounter(secondCounter + 1);
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Licznik nr 1 wynosi: {firstCounter}</p>
+      <p>Licznik nr 2 wynosi: {secondCounter}</p>
+      <Counter />
+      <button onClick={increaseFirstCounter}>
+        Zwiększ licznik nr 1
+      </button>
+      <button onClick={increaseSecondCounter}>
+        Zwiększ licznik nr 2
+      </button>
     </div>
   );
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Counter = () => {
+  console.log('render');
+
+  return (
+    <div>
+      Komponent nigdy się nie zmienia!
+    </div>
+  )
+}
+
+export default Counter;
