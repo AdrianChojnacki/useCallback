b62b069bc0024c88be95990c6321cd23416486f5
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-20 20:40:06 +0100

    object example

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 const App = () => {
   const [firstCounter, setFirstCounter] = useState(0);
   const [secondCounter, setSecondCounter] = useState(0);
+  const [state, setState] = useState({x: 0, y: 0});
 
   const increaseFirstCounter = () =>
     setFirstCounter(firstCounter + 1);
@@ -18,17 +19,20 @@ const App = () => {
   //   <Counter counter={firstCounter} index={1} />, [firstCounter]
   // );
 
+  const increaseX = () => setState({...state, x: state.x + 1});
+  const increaseY = () => setState({...state, y: state.y + 1});
+
   return (
     <div>
-      <p>Licznik nr 1 wynosi: {firstCounter}</p>
-      <p>Licznik nr 2 wynosi: {secondCounter}</p>
+      <p>Licznik nr 1 wynosi: {state.x}</p>
+      <p>Licznik nr 2 wynosi: {state.y}</p>
       {/* {firstCounterComponent} */}
-      <Counter counter={firstCounter} index={1} />
-      <Counter counter={secondCounter} index={2} />
-      <button onClick={increaseFirstCounter}>
+      <Counter counter={state.x} index={1} />
+      <Counter counter={state.y} index={2} />
+      <button onClick={increaseX}>
         Zwiększ licznik nr 1
       </button>
-      <button onClick={increaseSecondCounter}>
+      <button onClick={increaseY}>
         Zwiększ licznik nr 2
       </button>
     </div>
